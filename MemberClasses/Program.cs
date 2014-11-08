d72c237e8234df8cc926e266be83a82f556becea
mattLauter <spree33@live.com>
2014-11-08 16:51:23 -0600

    MemberClasses
    both member and member manage

---
+++ MemberClasses/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            memberManager.LoadNextMemberNumber(100000000);
+            memberManager.addMember("person1", "test", "test2", "MN", 55068);
+            memberManager.addMember("person2", "test", "test2", "MN", 55068);
+            memberManager.addMember("person3", "test", "test2", "MN", 55068);
+            memberManager.addMember("person4", "test", "test2", "MN", 55068);
+            memberManager.addMember("person5", "test", "test2", "MN", 55068);
+            memberManager.addMember("person6", "test", "test2", "MN", 55068);
+            memberManager.addMember("person7", "test", "test2", "MN", 55068);
+            memberManager.addMember("person8", "test", "test2", "MN", 55068);
+
+            Console.WriteLine(memberManager.validateMember(100000001));
+            memberManager.deleteMember(100000001);
+            Console.WriteLine(memberManager.validateMember(100000001));
+            Console.WriteLine(memberManager.validateMember(100000000));
+            memberManager.suspendMember(100000000);
+            Console.WriteLine(memberManager.validateMember(100000000));
+            memberManager.unSuspendMember(100000000);
+            Console.WriteLine(memberManager.validateMember(100000000));
+
+            Console.WriteLine(memberManager.getMember(100000004).getMemberName());
+            memberManager.editMember(100000004, "Edit1Test");
+            Console.WriteLine(memberManager.getMember(100000004).getMemberName());
+
+
+            Console.WriteLine(memberManager.getMember(100000004).getMemberStreetAddress());
+            Console.WriteLine(memberManager.getMember(100000004).getMemberCity());
+            Console.WriteLine(memberManager.getMember(100000004).getMemberState());
+            Console.WriteLine(memberManager.getMember(100000004).getZipCode());
+            memberManager.editMember(100000004, "street", "city","state",9999);
+            Console.WriteLine(memberManager.getMember(100000004).getMemberStreetAddress());
+            Console.WriteLine(memberManager.getMember(100000004).getMemberCity());
+            Console.WriteLine(memberManager.getMember(100000004).getMemberState());
+            Console.WriteLine(memberManager.getMember(100000004).getZipCode());
+
+
+        }
+    }
+}
---
+++ MemberClasses/memberManager.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberClasses
+{
+    static class memberManager
+    {
+        private static List<member> memberList = new List<member>();
+        private static int nextMemberNumber = 0;
+
+
+        public static void LoadNextMemberNumber(int number)
+        {
+            nextMemberNumber = number;
+        }
+
+        public static member getMember(int number)
+        {
+            foreach(member M in memberList)
+            {
+                if (M.getNumber() == number)
+                    return M;
+            }
+            return new member();
+        }
+
+        public static string validateMember(int number)
+        {
+            member checkMember = getMember(number);
+
+            if(checkMember.getMemberName() == null)//check this
+            {
+                return "Invalid number";
+            }
+            else
+            {
+                if (checkMember.getSuspended() == true)
+                    return "Member suspended";
+                else
+                    return "Validated";
+            }
+
+        }
+
+        public static void addMember(string name, string street, string city, string state, int zip)
+        {
+            member newMember = new member(nextMemberNumber, name, street, city, state, zip);
+            memberList.Add(newMember);
+            nextMemberNumber++;
+        }
+        public static void deleteMember(int number)
+        {
+            member memberToDelete = getMember(number);
+            memberList.Remove(memberToDelete);
+        }
+
+        public static void editMember(int number, string newName)
+        {
+            member memberToEdit = getMember(number);
+            if(validateMember(number) == "Validated")
+            {
+                memberToEdit.setMemberName(newName);
+            }
+        }
+
+        public static void editMember(int number, string newStreet,string newCity, string newState, int newZip )
+        {
+            member memberToEdit = getMember(number);
+            if (validateMember(number) == "Validated")
+            {
+                memberToEdit.setMemberStreetAddress(newStreet);
+                memberToEdit.setMemberCity(newCity);
+                memberToEdit.setMemberState(newStreet);
+                memberToEdit.setZipCode(newZip);
+            }
+        }
+
+
+        public static void suspendMember(int number)
+        {
+            member memberToSuspend = getMember(number);
+            if (validateMember(number) == "Validated")
+            {
+                memberToSuspend.setSuspended(true);
+            }
+        }
+
+        public static void unSuspendMember(int number)
+        {
+            member memberToUnsuspend = getMember(number);
+            if (validateMember(number) == "Member suspended")
+            {
+                memberToUnsuspend.setSuspended(false);
+            }
+        }
+
+    }
+}
