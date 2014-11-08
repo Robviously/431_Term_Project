d4832392270bc5c2c073369609de0baf0b13e2ca
ruhshod09 <ruhshod09@gmail.com>
2014-11-08 14:46:27 -0600

    first iteration

---
+++ Reports/Interfaces/IMemberReport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reports.Interfaces
+{
+    interface IMemberReport
+    {
+        void getWeeklyMembersReport(List<Member> memberList);
+        void getMemberReport(Member member);
+    }
+}
