78a55ea5d9cafb0df59b1ade2825c16ced361f03
Robviously <pero1002@stcloudstate.edu>
2014-11-08 20:55:03 -0600

    Initial Commit - UI, PizzaAnonymous, Provider, ProviderManager

---
+++ PizzaAnonymousApplication/PizzaAnonymousApplication/PizzaAnonymous.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaAnonymousApplication
+{
+    class PizzaAnonymous
+    {
+        private static PizzaAnonymous pizzaAnonymous;
+        private static ProviderManager providerManager;
+        //private static MemberManager memberManager;
+        //private static ServiceManager serviceManager;
+
+        private PizzaAnonymous()
+        {
+            providerManager = new ProviderManager();
+            //memberManager = new MemberManager();
+            //serviceManager = new ServiceManager();
+        }
+
+        public static PizzaAnonymous instance()
+        {
+            if (pizzaAnonymous == null)
+            {
+                return pizzaAnonymous = new PizzaAnonymous();
+            }
+            else
+            {
+                return pizzaAnonymous;
+            }
+        }
+
+        public void addProvider()
+        {
+            String name = UserInterface.getString("Enter the provider's name: ");
+            String streetAddress = UserInterface.getString("Enter the provider's street address: ");
+            String city = UserInterface.getString("Enter the provider's city: ");
+            String state = UserInterface.getString("Enter the provider's state: ");
+            int zipCode = UserInterface.getInteger("Enter the provider's ZIP code: ");
+
+            providerManager.addProvider(name, streetAddress, city, state, zipCode);
+
+            Console.Out.WriteLine("Success adding provider!");
+        }
+
+        public void printProviders()
+        {
+            String providersString = providerManager.toString();
+
+            if (providersString == "")
+            {
+                Console.Out.WriteLine("No providers in the database.");
+            }
+            else
+            {
+                Console.Out.WriteLine(providersString);
+            }
+        }
+
+        public void deleteProvider()
+        {
+            int providerId = UserInterface.getInteger("Enter the provider ID: ");
+
+            if (providerManager.validateProvider(providerId))
+            {
+                providerManager.deleteProvider(providerId);
+                Console.Out.WriteLine("Provider deleted.");
+            }
+            else
+            {
+                Console.Out.WriteLine("Unable to find provider.");
+            }
+        }
+
+        public void editProvider()
+        {
+            int providerId = UserInterface.getInteger("Enter provider ID: ");
+
+            if (providerManager.validateProvider(providerId))
+            {
+                String choice = UserInterface.getString("Enter field to edit(name or address): ");
+
+                if (choice.ToLower().Equals("name"))
+                {
+                    String name = UserInterface.getString("Enter provider name: ");
+
+                    providerManager.editProviderName(providerId, name);
+                    Console.Out.WriteLine("Provider name edited.");
+       
+                }
+                else if (choice.ToLower().Equals("address"))
+                {
+                    String streetAddress = UserInterface.getString("Enter provider street address: ");
+                    String city = UserInterface.getString("Enter provider city: ");
+                    String state = UserInterface.getString("Enter provider state: ");
+                    int zipCode = UserInterface.getInteger("Enter provider ZIP code: ");
+
+                    providerManager.editProviderAddress(providerId, streetAddress, city, state, zipCode);
+                    Console.Out.WriteLine("Provider address edited.");
+                }
+                else
+                {
+                    Console.Out.WriteLine("Unknown field entered. Valid fields: name, address.");
+                }
+            }
+            else
+            {
+                Console.Out.WriteLine("Unable to find provider");
+            }
+        }
+
+        public void addMember()
+        {
+            Console.Out.WriteLine("addMember Method");
+            /*
+            String name = UserInterface.getString("Enter the member's name: ");
+            String streetAddress = UserInterface.getString("Enter the member's street address: ");
+            String city = UserInterface.getString("Enter the member's city: ");
+            String state = UserInterface.getString("Enter the member's state: ");
+            int zipCode = UserInterface.getInteger("Enter the member's ZIP code: ");
+
+            memberManager.addMember(name, streetAddress, city, state, zipCode);
+
+            Console.Out.WriteLine("Success adding member!");
+             */
+        }
+
+        public void printMembers()
+        {
+            Console.Out.WriteLine("printMembers Method");
+            /*
+            String membersString = memberManager.toString();
+
+            if (membersString == "")
+            {
+                Console.Out.WriteLine("No members in the database.");
+            }
+            else
+            {
+                Console.Out.WriteLine(membersString);
+            }
+             * */
+        }
+
+        public void deleteMember()
+        {
+            Console.Out.WriteLine("deleteMember Method");
+            /*
+            int memberId = UserInterface.getInteger("Enter the member ID: ");
+
+            if (memberManager.validateMember(memberId))
+            {
+                memberManager.deleteMember(memberId);
+                Console.Out.WriteLine("Member deleted.");
+            }
+            else
+            {
+                Console.Out.WriteLine("Unable to find member");
+            }
+             * */
+        }
+
+        public void editMember()
+        {
+            Console.Out.WriteLine("editMember Method");
+            /*
+            int memberId = UserInterface.getInteger("Enter provider ID: ");
+
+            if (memberManager.validateMember(memberId))
+            {
+                String choice = UserInterface.getString("Enter field to edit(name or address): ");
+
+                if (choice.ToLower().Equals("name"))
+                {
+                    String name = UserInterface.getString("Enter member name: ");
+
+                    memberManager.editMemberName(memberId, name);
+                    Console.Out.WriteLine("Member name edited.");
+
+                }
+                else if (choice.ToLower().Equals("address"))
+                {
+                    String streetAddress = UserInterface.getString("Enter member street address: ");
+                    String city = UserInterface.getString("Enter member city: ");
+                    String state = UserInterface.getString("Enter member state: ");
+                    int zipCode = UserInterface.getInteger("Enter member ZIP code: ");
+
+                    memberManager.editMemberAddress(memberId, streetAddress, city, state, zipCode);
+                    Console.Out.WriteLine("Member address edited.");
+                }
+                else
+                {
+                    Console.Out.WriteLine("Unknown field entered. Valid fields: name, address.");
+                }
+            }
+            else
+            {
+                Console.Out.WriteLine("Unable to find member.");
+            }
+             */
+        }
+
+        public void addService()
+        {
+            Console.Out.WriteLine("addService Method");
+            /*
+            String name = UserInterface.getString("Enter the service's name: ");
+            float fee = UserInterface.getFloat("Enter the service's fee: ");
+            String description = UserInterface.getString("Enter the service's description: ");
+
+            serviceManager.addService(name, fee, description);
+            Console.Out.WriteLine("Success adding service!");
+            */
+        }
+
+        public void printServices()
+        {
+            Console.Out.WriteLine("printServices Method");
+            /*
+            String servicesString = serviceManager.toString();
+
+            if (servicesString == "")
+            {
+                Console.Out.WriteLine("No services in the database.");
+            }
+            else
+            {
+                Console.Out.WriteLine(servicesString);
+            }
+            */
+        }
+
+        public void deleteService()
+        {
+            Console.Out.WriteLine("deleteService Method");
+            /*
+            int serviceId = UserInterface.getInteger("Enter the service ID: ");
+
+            if (serviceManager.validateService(serviceId))
+            {
+                serviceManager.deleteService(serviceId);
+                Console.Out.WriteLine("Service deleted.");
+            }
+            else
+            {
+                Console.Out.WriteLine("Unable to find service");
+            }
+            */
+        }
+
+        public void editService()
+        {
+            Console.Out.WriteLine("editService Method");
+            /*
+            int serviceId = UserInterface.getInteger("Enter service ID: ");
+
+            if (serviceManager.validateService(serviceId))
+            {
+                String choice = UserInterface.getString("Enter field to edit(name, fee, description): ");
+
+                if (choice.ToLower().Equals("name"))
+                {
+                    String name = UserInterface.getString("Enter service name: ");
+
+                    serviceManager.editServiceName(serviceId, name);
+                    Console.Out.WriteLine("Service name edited.");
+                }
+                else if (choice.ToLower().Equals("fee"))
+                {
+                    float fee = UserInterface.getFloat("Enter service fee: ");
+
+                    serviceManager.editServiceFee(serviceId, fee);
+                    Console.Out.WriteLine("Service fee edited.");
+                }
+                else if (choice.ToLower().Equals("description"))
+                {
+                    String description = UserInterface.getString("Enter service description");
+
+                    serviceManager.editServiceDescription(serviceId, description);
+                    Console.Out.WriteLine("Service description edited.");
+                }
+                else
+                {
+                    Console.Out.WriteLine("Unknown field entered. Valid fields: name, fee, description");
+                }
+            }
+            else
+            {
+                Console.Out.WriteLine("Unable to find service");
+            }
+            */
+        }
+    }
+}
---
+++ PizzaAnonymousApplication/PizzaAnonymousApplication/UserInterface.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaAnonymousApplication
+{
+    class UserInterface
+    {
+        private static UserInterface userInterface;
+        private static PizzaAnonymous pizzaAnonymous;
+
+        private UserInterface()
+        {
+            pizzaAnonymous = PizzaAnonymous.instance();
+        }
+
+        public static UserInterface instance()
+        {
+            if (userInterface == null)
+            {
+                return userInterface = new UserInterface();
+            }
+            else
+            {
+                return userInterface;
+            }
+        }
+
+        public void displayMainMenu()
+        {
+            bool done = false;		
+            int choice;				    
+        
+            while (!done)
+            {
+                Console.Out.WriteLine("                                     ");
+                Console.Out.WriteLine("        MAIN MENU                    ");
+                Console.Out.WriteLine("        Pizza-anonymous              ");
+                Console.Out.WriteLine("                                     ");
+                Console.Out.WriteLine("        1. Display Manager Menu      ");
+                Console.Out.WriteLine("        2. Display Provider Menu     ");
+                Console.Out.WriteLine("        3. Quit                      ");
+                Console.Out.WriteLine("                                     ");
+
+                choice = getInteger("Enter your choice: ");
+
+                switch (choice)
+                {
+                    case 1:   done = true;
+                		      displayManagerMenu();
+                              break;
+
+                    case 2:   done = true;
+                	          displayProviderMenu();
+                              break;
+
+                    case 3:   done = true;
+                              break;
+
+                    default:  Console.Out.WriteLine("Not a valid choice! Please try again.\n");
+                              break;     
+                } 
+            }   
+        }
+
+        private void displayManagerMenu ()
+        {
+            bool done = false;		
+            int choice;				    
+        
+            while (!done)
+            {
+                Console.Out.WriteLine("                                     ");
+        	    Console.Out.WriteLine("        MANAGER MENU                 ");
+        	    Console.Out.WriteLine("        Pizza-anonymous              ");
+        	    Console.Out.WriteLine("                                     ");
+        	    Console.Out.WriteLine("        1. Add Member                ");
+        	    Console.Out.WriteLine("        2. Edit Member               ");
+        	    Console.Out.WriteLine("        3. Delete Member             ");
+        	    Console.Out.WriteLine("        4. Add Provider              ");
+                Console.Out.WriteLine("        5. Edit Provider             ");
+                Console.Out.WriteLine("        6. Delete Provider           ");
+                Console.Out.WriteLine("        7. Add Service               ");
+                Console.Out.WriteLine("        8. Edit Service              ");
+                Console.Out.WriteLine("        9. Delete Service            "); 
+                Console.Out.WriteLine("       10. Show Members              "); 
+                Console.Out.WriteLine("       11. Show Providers            "); 
+                Console.Out.WriteLine("       12. Show Services             "); 
+                Console.Out.WriteLine("       13. Display Main Menu         ");
+                Console.Out.WriteLine("                                     ");
+
+                choice = getInteger("Enter your choice: ");
+         
+                switch (choice)
+                {
+                    case  1:   	pizzaAnonymous.addMember();
+                           	    break;
+
+                    case  2:   	pizzaAnonymous.editMember();
+                           	    break;
+
+                    case  3:   	pizzaAnonymous.deleteMember();
+                           	    break; 
+                           
+                    case  4:   	pizzaAnonymous.addProvider();
+                           	    break;  
+                           
+                    case  5:   	pizzaAnonymous.editProvider();
+                           	    break; 
+                           
+                    case  6:   	pizzaAnonymous.deleteProvider();
+                           	    break;            
+                           
+                    case  7:   	pizzaAnonymous.addService();
+                           	    break;            
+                           
+                    case  8:   	pizzaAnonymous.editService();
+                           	    break;            
+                           
+                    case  9:   	pizzaAnonymous.deleteService();
+                           	    break;  
+                           
+                    case 10:   	pizzaAnonymous.printMembers();
+                			    break;
+                			
+                    case 11:   	pizzaAnonymous.printProviders();
+    						    break;
+    						
+                    case 12:   	pizzaAnonymous.printServices();
+    						    break;
+                           
+                    case 13:   	done = true;
+     		   				    displayMainMenu();
+     		   				    break;    
+
+                    default:   	Console.Out.WriteLine("Not a valid choice! Please try again.\n");
+                           	    break;     
+                }     
+            }  
+        }
+
+        private void displayProviderMenu ()
+        {
+            bool done = false;
+            int choice;				    
+        
+            while (!done)
+            {
+                Console.Out.WriteLine("                                     ");
+        	    Console.Out.WriteLine("      PROVIDER MENU                  ");
+        	    Console.Out.WriteLine("      Pizza-anonymous                ");
+        	    Console.Out.WriteLine("                                     ");
+        	    Console.Out.WriteLine("      1. Authenticate Member         ");
+        	    Console.Out.WriteLine("      2. Provide Service             ");
+        	    Console.Out.WriteLine("      3. Generate Report             ");
+        	    Console.Out.WriteLine("      4. Display Main Menu           ");
+                Console.Out.WriteLine("                                     ");
+
+                choice = getInteger("Enter your choice: ");
+         
+                switch (choice)
+                {
+                    case  1:   Console.Out.WriteLine("Authenticate Member Method"); //authenticateMember();
+                               break;
+
+                    case  2:   Console.Out.WriteLine("Provide Service Method"); //provideService();
+                               break;
+                            
+                    case  3:   Console.Out.WriteLine("Generate Report Method"); //generateReport();
+                               break;
+
+                    case  4:   done = true;
+                			   displayMainMenu();
+                               break;
+
+                    default:   Console.Out.WriteLine("Not a valid choice! Please try again.\n");
+                               break;     
+                }    
+            }  
+        }
+
+        public static String getString(String prompt)
+	    {
+            Console.Out.WriteLine(prompt);
+            return Console.In.ReadLine(); 
+	    }
+
+        public static int getInteger(String prompt)
+	    {
+            int number;
+
+            Console.Out.Write(prompt);
+            String input = Console.ReadLine();
+
+            while (!Int32.TryParse(input, out number))
+            {
+                Console.WriteLine("Please enter an integer: ");
+                input = Console.ReadLine();
+            }
+            return number;
+	    }
+
+        public static float getFloat(String prompt)
+	    {
+            float number;
+
+            Console.Out.Write(prompt);
+            String input = Console.ReadLine();
+
+            while (!float.TryParse(input, out number))
+            {
+                Console.WriteLine("Please enter a decimal: ");
+                input = Console.ReadLine();
+            }
+            return number;
+	    }
+
+        public bool yesOrNo(String prompt)
+        {
+            String more = getString(prompt + " (Y|y)[es] or anything else for no");
+            if (more.ElementAt(0) != 'y' && more.ElementAt(0) != 'Y')
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
